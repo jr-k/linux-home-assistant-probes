c417b4c969c6deadad2648b357aa7e934cb48334
JRK <jessym.reziga@gmail.com>
2019-11-04 02:10:36 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+function get_server_cpu_usage(){
+    $load = sys_getloadavg();
+    return round($load[0]*100).'%';
+}
+
+function get_server_memory_usage(){
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = ($mem[2]/$mem[1])*100;
+    return round($memory_usage).'%';
+}
+
+function get_server_storage_usage() {
+    $df = shell_exec('df');
+    $df = (string)trim($df);
+    $df_arr = explode("\n", $df);
+    $storage = explode(" ", $df_arr[1]);
+    return $storage[12];
+}
+
+function get_server_temperature() {
+    $tp = shell_exec('cat /sys/class/thermal/thermal_zone0/temp');
+    return round($tp*0.001).'°C';
+}
+
+Header('Content-Type: application/json');
+
+$response = array(
+        'cpu' => get_server_cpu_usage(),
+        'ram' => get_server_memory_usage(),
+        'disk' => get_server_storage_usage(),
+        'temp' => get_server_temperature()
+);
+
+echo json_encode($response);

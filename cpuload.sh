9c48ed6588579085f5093ccf1d9c6171c5e20c7f
JRK <jessym.reziga@gmail.com>
2019-11-05 02:33:39 +0100

    Update and rename cputemp.sh to cpuload.sh

--- cpuload.sh
+++ cpuload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 temp=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) ""; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
-echo $temp > /home/cputemp
+echo $temp > /home/cpuload
